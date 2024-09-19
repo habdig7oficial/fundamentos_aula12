574029ebce6de8ca6e822d88c3f43d05a48ee294
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2024-09-19 18:41:35 -0300

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,4 +1,5 @@
-values= "0.23,0.4"
+values= "0.80, 0.12, 0.01, 0.22, 0.35, 0.25, 0.77, 0.50, 0.53, 0.45"
+n = 5
 
 values = values.split(",")
 
@@ -7,3 +8,13 @@ for value, i in zip(values,  range(len(values))):
     correspondency[str(i)] = float(value)
     print(value)
 print(correspondency)
+
+ordered = dict(sorted(correspondency.items(), reverse = True, key= lambda item: item[1]))
+print(ordered)
+
+res = []
+for i in ordered:
+    res.append(i)
+
+p = "".join(res)
+print(p[0:n])
